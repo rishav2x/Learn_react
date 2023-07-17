f7b3529cf1ea795e70661d831cdb5816ee35f1d7
rishav2x <rishavkumar2x@gmail.com>
2023-07-17 18:04:43 +0530

    react func compo

--- App.js
+++ App.js
@@ -1,37 +1,13 @@
-/* const heading = React.createElement(
-    "h1",
-    { id: "heading",xyz: "abc"},
-    "Hello World from React!"
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+//React Functional Component
+const HeadingComponent = () => (
+    <div id="container">
+<h1 className="heading">Nameste React Functional Component</h1>
+    </div>
 );
-*/
-/*
-<div id="parent">
-  <div id="child">
-    <h1>I am h1 tag</h1>
-    <h2>I am h2 tag</h2>
-  </div>
-  <div id="child2">
-    <h1>I am h1 tag</h1>
-    <h2>I am h2 tag</h2>
-  </div>
-</div>;
-
-
-*/
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
-]),
-]);
-
-console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
