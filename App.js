cc50bf662f53f5de136702943e0d81459965f749
Rishav <rishavkumar2x@gmail.com>
2023-07-19 00:22:53 +0530

    Header compo

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const Title = () => (
-<h1>Nameste </h1>
-);
-//React Functional Component --- React composite Component
-const HeadingComponent = () => (
-    <div id="container">
-      <Title />
-<h1 className="heading">Nameste React Functional Component</h1>
-    </div>
-);
+const Header = () => {
+    return (
+      <div className="header">
+        <div>
+          <img
+            className="logo"
+            src="https://www.logodesign.net/logo/food-cloche-2836ld.png?nwm=1&nws=1&industry=food&sf="
+            alt=""
+            
+          />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+}
+
+const AppLayout = () => {
+    return (
+        <div className ="app">
+<Header />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
