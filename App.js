f9d481daf44f9c7034bccb7d36e68610a0badba7
Rishav <rishavkumar2x@gmail.com>
2023-07-19 23:04:55 +0530

    resturantcard

--- App.js
+++ App.js
@@ -1,35 +1,65 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div>
-          <img
-            className="logo"
-            src="https://www.logodesign.net/logo/food-cloche-2836ld.png?nwm=1&nws=1&industry=food&sf="
-            alt=""
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/food-cloche-2836ld.png?nwm=1&nws=1&industry=food&sf="
+          alt=""
+        />
       </div>
-    );
-}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const ResturantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://media.istockphoto.com/id/1086974196/photo/keema-or-kheema-biryani-fragrant-and-spicy-minced-lamb-or-goat-or-chicken-cooked-in-range-of.jpg?s=612x612&w=0&k=20&c=CSx2OuaMm0NtRKcDvCx0yME8Dd5J7OdNYuKCQME6cMk="
+        alt="res-logo"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+        <ResturantCard />
+        
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
-    return (
-        <div className ="app">
-<Header />
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
