74a6b329403e68d702e2e3176853f71adfb40332
Rishav <rishavkumar2x@gmail.com>
2023-07-21 01:07:20 +0530

    Folder structuring

---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+import ResturantCard from "./ResturantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("Button Clicked");
+          }}
+        >
+          Top Rated Resturants
+        </button>
+      </div>
+      <div className="res-container">
+        {resList.map((resturant) => (
+          <ResturantCard key={resturant.info.id} resData={resturant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/food-cloche-2836ld.png?nwm=1&nws=1&industry=food&sf=";
