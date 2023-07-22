f0c2342296102e7d677e8e88b1bae24efde472bd
Rishav <rishavkumar2x@gmail.com>
2023-07-23 03:10:38 +0530

    api calling data coming from live website

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,26 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   // local state variable
-  const [listOfResturants, setListOfResturant] = useState(resList);
+  const [listOfResturants, setListOfResturant] = useState([]);
+
+  //api caling
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    setListOfResturant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
   return (
     <div className="body">
       <div className="filter">
