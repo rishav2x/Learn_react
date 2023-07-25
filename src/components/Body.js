3c0001d8e133a22e06035945cd3dd003e83a6999
Rishav <rishavkumar2x@gmail.com>
2023-07-26 01:02:33 +0530

    search box

--- src/components/Body.js
+++ src/components/Body.js
@@ -28,6 +28,10 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input type="text" className="search-box" />
+          <button>Search</button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
