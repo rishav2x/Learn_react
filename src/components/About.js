992dc8de2549f03990417d95e7588fd526b26bf9
Rishav <rishavkumar2x@gmail.com>
2023-08-15 01:03:41 +0530

    working on class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
+import User from "./user";
+
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <h2>This is About Page.</h2>
+      <User name={"Rishav kumar (func)"} />
     </div>
   );
 };
